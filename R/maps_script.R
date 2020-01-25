63dca1bfdf0112867b4899ed36270b7f6f6105a3
skhaji <sara@edbuild.org>
2020-01-25 14:53:31 -0500

    created loop to create map for each election district, with title.  Just need to write it to appropriate folder when we define the file structure

--- R/maps_script.R
+++ R/maps_script.R
@@ -1,12 +1,9 @@
 ### This script works to loop through a shapefile and create a map of each polygon
-### It is looping through states, but we can replace with EDs
-### We'll need to add the leaflet package so that there is a street map layer
-### otherwise it should work with any shapefile
 
 setwd("/Users/Hodges/Desktop/ryb")
 
 
-#### probably don't need all these packages
+#### probably don't need all these packages, check later
 require(sf)
 require(dplyr)
 require(mapview)
@@ -30,14 +27,11 @@ bk_ed_shp <- ed_shp %>%
 
 ad56_27 <- bk_ed_shp %>% 
   mutate(elect_dist = as.character(elect_dist)) %>% 
-  filter(elect_dist == "56027" | elect_dist == "51082" |
-           elect_dist == "45003")
+  filter(elect_dist == "56027") 
 
-map_leaflet <- leaflet() %>%
-  addTiles() %>%
-  addControl(title, position = "topleft", className="map-title")
-
-#### tester with ad56_27
+#############################
+#### tester with ad56_27 ###
+#############################
 temp_2_map <- tm_basemap("CartoDB.Voyager") +
   tm_shape(ad56_27) +
   tm_borders(lwd=3, col = "red", alpha = 1) +
@@ -45,51 +39,43 @@ temp_2_map <- tm_basemap("CartoDB.Voyager") +
     frame = FALSE) ## remove black border frame
 
 lf <- tmap_leaflet(temp_2_map) %>% 
-  addControl("test", position = "topright")
+  addControl("test", position = "topright") ### adds title
 
+### writes out an image of the leaflet map above
 mapshot(lf, file = "test6.png")
 
-#### creating the data frame to loop through
-# states <- as.data.frame(dismissed_points) %>%
-#   select(-geometry) %>% 
-#   select(Postal) %>%
-#   distinct()
+#############################
+######## END TESTING #######
+#############################
 
-setwd("~/Box Sync/Data/Dismissed/Visuals/Infographics/one_pager_maps")
+#### ad_ed_list is the dataframe to loop through - use this first  if you want to create new maps for a few districts
+aded_list <- ad_ed_list %>%
+  filter(ad_ed == "56027" | ad_ed == "51082" |
+           ad_ed == "45003")
 
-### this is the list of the states to loop through below
-# states_list <- as.list(dplyr::pull(ad_ed_list, ad_ed))
-ad56_27_list <- as.data.frame(ad56_27) %>% 
-  select(-geometry) %>% 
-  select(elect_dist)
+ad_ed <- as.list(pull(aded_list, ad_ed)) ### turns ad ed list into list to loop thorugh
 
-ed_list <- as.list("56027", "51082", "45003") 
+#### define 
+# setwd("/Users/Hodges/Desktop/ryb")
 
-for (ed in ed_list){
+for (ed in ad_ed){
+  shape <- bk_ed_shp %>% 
+    filter(elect_dist == ed)  ### filters the appropriate election district
   
-  # temp_border <- state_border %>% 
-  #   filter(STUSPS == state)
-  temp_border <- ad56_27 %>%
-    filter(elect_dist == ed)
-  # 
-  # temp_points <- dismissed_points %>% 
-  #   filter(Postal == state)
+  ed_title <- paste0("AD ", substr(ed, 0, 2), " ED ", substr(ed, 3, 5))  ### creates title
   
   temp_2_map <- tm_basemap("CartoDB.Voyager") +
-    tm_shape(temp_border) +
-    tm_borders(lwd=1.5, col = "#008ca3", alpha = .5) +
+    tm_shape(shape) +
+    tm_borders(lwd=3, col = "red", alpha = 1) +
     tm_layout(frame = FALSE) ## remove black border frame
 
-  filename_ed <- paste0(ed, "_map.png")
-  
-  lf <- tmap_leaflet(temp_2_map)
-  mapshot(lf, file = filename_ed)
-  
-  #ggsave(g2, filename = filename_state,  bg = "transparent", units = "in", width = 5, height = 8)
-  # tmap_save(temp_2_map, filename = filename_state, units = "in", dpi=200, width = 4.46)
-}
+  lf <- tmap_leaflet(temp_2_map) %>%
+    addControl(ed_title, position = "topright") ### adds title
 
-#tmap_save(state_map, "al_map.png", units = "in", dpi=200, width = 4.46)
+  ### writes out an image of the leaflet map above
+  filename_aded <- paste0("/Users/Hodges/Desktop/ryb/maps/", ed, "_map.png")
+  mapshot(lf, file = filename_aded)
+}
 
 
 #### That's it!
